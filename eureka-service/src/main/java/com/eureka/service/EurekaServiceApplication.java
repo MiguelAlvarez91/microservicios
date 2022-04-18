4a2819fb7e70c8fd839f3a550b2f3f5562443ffb
Alvarez aguilera <s3794116@mexeo.bns>
2022-04-18 15:29:41 -0500

    eureka

--- eureka-service/src/main/java/com/eureka/service/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/eureka/service/EurekaServiceApplication.java
@@ -1,15 +1,16 @@
-package com.moto.service;
+package com.eureka.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class MotoServiceApplication {
+@EnableEurekaServer
+public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MotoServiceApplication.class, args);
+		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
 
 }
